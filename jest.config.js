f090952a682cfe4b3ff5064180b7fab0bbcce107
Marco Cesi <sq@iamsquare.it>
2019-10-19 10:50:01 +0200

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  setupFiles: [],
+  setupFilesAfterEnv: [],
+  moduleFileExtensions: ['js', 'json', 'node'],
+  moduleNameMapper: {},
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.js', '!**/node_modules/**']
+};
